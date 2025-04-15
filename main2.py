a21a87955d7d73c38714a0972f450ab92bd810d8
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-04-15 11:50:42 +0530

    fixed downloading error

--- main2.py
+++ main2.py
@@ -1,30 +1,30 @@
-import tkinter
-import customtkinter
-from pytubefix import YouTube
+# import tkinter
+# import customtkinter
+# from pytubefix import YouTube
 
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
+# class App(customtkinter.CTk):
+#     def __init__(self):
+#         super().__init__()
 
-        self.title("My app")
-        self.geometry("400x150")
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure((0, 1), weight=1)
+#         self.title("My app")
+#         self.geometry("400x150")
+#         self.columnconfigure(0, weight=1)
+#         self.rowconfigure((0, 1), weight=1)
 
-        self.checkboxfrane = customtkinter.CTkFrame(self)
-        self.checkboxfrane.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
-        self.checkbox2 = customtkinter.CTkCheckBox(self, text="two")
-        self.checkbox2.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox3 = customtkinter.CTkCheckBox(self, text="Three")
-        self.checkbox3.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+#         self.checkboxfrane = customtkinter.CTkFrame(self)
+#         self.checkboxfrane.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+#         self.checkbox2 = customtkinter.CTkCheckBox(self, text="two")
+#         self.checkbox2.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+#         self.checkbox3 = customtkinter.CTkCheckBox(self, text="Three")
+#         self.checkbox3.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
 
-        self.button = customtkinter.CTkButton(self, text="Press me", command=self.button_pressed)
-        self.button.grid(row = 3, column=0, padx=10, pady=(0, 10), sticky="ew")
+#         self.button = customtkinter.CTkButton(self, text="Press me", command=self.button_pressed)
+#         self.button.grid(row = 3, column=0, padx=10, pady=(0, 10), sticky="ew")
 
-    def button_pressed(self):
-        return "press button"
+#     def button_pressed(self):
+#         return "press button"
 
 
 
-app = App()
-app.mainloop()
+# app = App()
+# app.mainloop()
